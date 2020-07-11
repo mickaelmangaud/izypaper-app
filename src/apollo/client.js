3988bff3621abc18fae872b04fd9f2b6a769af31
Mickael Mangaud <mickaelmangaud@gmail.com>
2020-07-12 00:39:09 +0200

    test buttons

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -3,11 +3,8 @@ import { ApolloClient, HttpLink, InMemoryCache, ApolloProvider } from '@apollo/c
 
 // test env
 const link = new HttpLink({
-  uri: `${process.env.REACT_APP_BASE_API_URL}`,
+  uri: `${process.env.REACT_APP_BASE_API_URL}/graphql`,
   credentials: 'include',
-  headers: {
-    "Access-Control-Allow-Credentials" : true
-  }
 });
 
 const client = new ApolloClient({
--- src/screens/login/Login.jsx
+++ src/screens/login/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { useQuery, gql } from '@apollo/client';
 import { Context } from '../../context/ContextProvider';
+import axios from 'axios';
 
 export const Login = () => {
   const { data } = useQuery(APOLLO_QUERY);
@@ -16,6 +17,23 @@ export const Login = () => {
             Déjà inscrit: {user.email}
           </p>
       )}
+      <button onClick={() => {
+        axios.post(`${process.env.REACT_APP_BASE_API_URL}/auth/login`, {
+          email: 'mickaelmangaud@gmail.com',
+          password: 'okcomputer'
+        },
+        { withCredentials: true}
+        )
+          .then(data => console.log(data))
+      }}>
+        Login
+      </button>
+      <button onClick={() => {
+        axios.get(`${process.env.REACT_APP_BASE_API_URL}/auth/user`, { withCredentials: true })
+          .then(data => console.log(data))
+      }}>
+        getUser
+      </button>
     </div>
   )
 };
