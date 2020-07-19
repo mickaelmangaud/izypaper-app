6066b25c59ed0eff2d12ef83e5709663aabd23bf
Mickael Mangaud <mickaelmangaud@gmail.com>
2020-07-19 10:31:14 +0200

    errors

--- src/screens/login/validate.js
+++ src/screens/login/validate.js
@@ -2,14 +2,7 @@ import { regex } from '../../utils';
 
 export const validateLogin = credentials => {
   let errors = {};
-
-  if (!regex.EMAIL.test(credentials.email)) {
-    errors.email = 'Email invalide'
-  } 
-
-  if (credentials.password.length < 6) {
-    errors.password = 'Mot de passe invalide'
-  }
-
+  errors.emails = !regex.EMAIL.test(credentials.email);
+  errors.password = credentials.password.length < 6;
   return errors;
 }
