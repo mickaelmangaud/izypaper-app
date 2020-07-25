06e27f2266025069bc30210572b2b894770854df
mma <mickael.mangaud@qualia.fr>
2020-07-25 23:30:49 +0200

    fonts

--- src/index.js
+++ src/index.js
@@ -4,17 +4,15 @@ import { App } from './core';
 import WebFont from 'webfontloader';
 import * as serviceWorker from './serviceWorker';
 
-/* Load fonts : https://github.com/typekit/webfontloader */
+/* Loads fonts : https://github.com/typekit/webfontloader */
 WebFont.load({
   google: {
     families: [
-      'Nunito Sans:300,400,700',
       'Ubuntu:300,400,700',
       'Lato:400',
       'Boogaloo',
       'Merienda One',
       'Material+Icons',
-      'sans-serif'
     ]
   }
 });
