d6871076a54882acc22f4e8902b7ce15a7cd9103
Mickael Mangaud <mickaelmangaud@gmail.com>
2020-07-29 11:49:09 +0200

    BIentot

--- src/core/Router.js
+++ src/core/Router.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { Switch, Route, Redirect, BrowserRouter } from 'react-router-dom';
 import { Login, Dashboard, NotFound404, CreateBusiness, Register } from '../screens';
 import { Context } from '../context';
+import { Soon } from '../screens/Soon';
 
 const AuthRoute = ({ component: Component, ...rest }) => {
   const { state } = useContext(Context);
@@ -19,10 +20,11 @@ export const AppRouter = () => {
   return(
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={Login} />
-        <Route exact path="/inscription" component={Register} />
+        {/* <Route exact path="/" component={Login} /> */}
+        <Route exact path="/" component={Soon} />
+        {/* <Route exact path="/inscription" component={Register} />
         <Route exact path="/creer-mon-entreprise" component={CreateBusiness} />
-        <AuthRoute exact path="/dashboard" component={Dashboard} />
+        <AuthRoute exact path="/dashboard" component={Dashboard} /> */}
         <Route path="*" component={NotFound404} />
       </Switch>
     </BrowserRouter>
---
+++ src/screens/Soon.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const Soon = () => {
+    return (
+        <div style={{
+            backgroundColor: '#048b9a',
+            height: '100vh',
+            width: '100vw',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            fontFamily: 'Ubuntu, sans-serif',
+            fontSize: '120px',
+            color: 'white'
+        }}>
+            Bientot
+        </div>
+    )
+}
